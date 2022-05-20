caad0eb18cb20a35802292d793312d061997ae46
Evgeni B <105727330+pintik129@users.noreply.github.com>
2022-05-21 03:17:19 +0500

    Update hello.php
    подправил, дз 2

--- hello.php
+++ hello.php
@@ -1,11 +1,16 @@
+<!DOCTYPE html>
 <html>
-<body>
-
-Здравствуй, мой дорогой друг, <?php echo $_GET["name"]; ?>!<br>
-Это здорово, что ты живешь в таком городе, как <?php echo $_GET["email"]; ?>!<br>
+ <head>
+   <title>Главная страница</title>
+   <meta charset="utf-8">
+ </head>
+ <body>
+Здравствуй, мой дорогой друг, <?php echo $_GET["name"]; //Имя, введеное пользователем?>!<br>
+Это здорово, что ты живешь в таком городе, как <?php echo $_GET["city"]; ?>!<br>
 И ты выглядишь слишком молодо для своих <?php echo $_GET["age"]; ?>! <br>
 Улыбайся всегда, прямо как и сейчас!
 <p><img src="https://memepedia.ru/wp-content/uploads/2019/12/screenshot_22-1.png" alt="Тут должен быть улыбающийся котик"></p>
+<!-- Изображение кота -->
 
 </body>
 </html>
