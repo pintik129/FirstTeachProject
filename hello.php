224cb9223a52771df00daa1ffe035ef1551ae57d
Evgeni B <105727330+pintik129@users.noreply.github.com>
2022-05-21 02:42:20 +0500

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Здравствуй, мой дорогой друг, <?php echo $_GET["name"]; ?>!<br>
+Это здорово, что ты живешь в таком городе, как <?php echo $_GET["email"]; ?>!<br>
+И ты выглядишь слишком молодо для своих <?php echo $_GET["age"]; ?>! <br>
+Улыбайся всегда, прямо как и сейчас!
+<p><img src="https://memepedia.ru/wp-content/uploads/2019/12/screenshot_22-1.png" alt="Тут должен быть улыбающийся котик"></p>
+
+</body>
+</html>
